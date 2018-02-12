89463f90a85ba15a9b35e598d30e42968d839b2e
nick2874 <nick2874@hotmail.com>
2018-02-11 18:33:08 -0700

    test

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+namespace Nick;
+ini_set("display_errors","1");
+error_reporting(E_ALL);
+
+require 'vendor/autoload.php';
+
+{// $obj=new A\A;
+// echo "<BR><HR>";
+// $obj=new B\A;
+// echo "<BR><HR>";
+// $obj=new C\A;
+}
+
+$core=new Libraries\Core;
+
